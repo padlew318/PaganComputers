17455927b3fd433f2fb595b2de093a5498cd7fcb
Paddy <Paddy@SHERMAN>
2016-04-16 21:55:36 +0100

    Add project files.

---
+++ PaganComputers/Controllers/TutorialsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaganComputers.Controllers
+{
+    public class TutorialsController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Tutorials.";
+
+            return View();
+        }
+
+        public ActionResult Git()
+        {
+            ViewBag.Message = "GIT.";
+
+            return View();
+        }
+
+        public ActionResult Linux()
+        {
+            ViewBag.Message = "Linux.";
+
+            return View();
+        }
+
+        public ActionResult VM()
+        {
+            ViewBag.Message = "VM.";
+
+            return View();
+        }
+
+    }
+}
